7820ed2e6c5bcbf0432b723dea89d548396ea60e
blancadennis <lblancadennis@gmail.com>
2020-12-17 22:13:00 -0600

    estilos listos, cifrado listo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,31 @@
+const cifrado = (desplazamiento,texto) => { //ARROW FUNCTION es mas corta el () => se sustituye por el function () {}//
+ 
+
+  const textoIngresado = texto.value;
+  //texto ingresado (el que el usuario ingresa, el split es para que cada que haya nada, o sea entre letra y letra, repita todo, que lo aplique en cada caracter valorEntero=Valor de Entrada al map)
+  return textoIngresado.split('').map(l => {
+    
+    let valorEntero = l.charCodeAt(0);
+      if (valorEntero >= 65 && valorEntero <= 90) {
+        const valorDesplazamiento = parseInt(desplazamiento.value);
+
+      if (valorEntero + valorDesplazamiento > 90)
+        valorEntero = 65 + (valorEntero - 90) + valorDesplazamiento - 1;
+
+      else
+        valorEntero = valorEntero + valorDesplazamiento;
+    }
+
+    let cifrado = String.fromCharCode(valorEntero);
+    return cifrado;
+    }).join("");
+
+  }
+
+
 const cipher = {
-  // ...
+  cifrado, 
+  descifrado:null
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+const desplazamiento = document.getElementById("offsetNumero")
+const textoCifrado = document.getElementById("mensajeYaCifrado")
+const textoSinCifrar = document.getElementById("mensajeParaCifrar")
+
+const btnCifrar = document.getElementById("btnCifrar")
+
+if (btnCifrar !== null){
+    btnCifrar.addEventListener("click", miFuncionCifrar) 
+}
+
+
+function miFuncionCifrar() {
+  const texto = cipher.cifrado(desplazamiento, textoSinCifrar)
+  textoCifrado.innerHTML = texto
+}
+
+
+
+
+
+//console.log(mensajeYaCifrado);
